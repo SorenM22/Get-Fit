b23d905c9d88b7d9ad40ba955e81c57b9e97bd60
Parker Vetter <vette107@d.umn.edu>
2024-10-22 14:48:50 -0500

    added base for Goal and Workout Tab in main

--- lib/main.dart
+++ lib/main.dart
@@ -43,15 +43,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _selectedPage = 0;
+  int _selectedWorkoutOrGoal = 0;
   String _username = 'USER NAME';
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
       _counter++;
     });
   }
@@ -69,6 +66,12 @@ class _MyHomePageState extends State<MyHomePage> {
   void _pressedAddButton() {
     throw UnimplementedError("Unimplemented _pressedAccountButton");
   }
+
+  void _pressedWorkoutOrGoal(int index) {
+    setState(() {
+      _selectedWorkoutOrGoal = index;
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -80,12 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         leading: TextButton(
             onPressed: _pressedAccountButton,
@@ -93,24 +91,17 @@ class _MyHomePageState extends State<MyHomePage> {
             isSemanticButton: true,),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            NavigationBar(
+              destinations: [
+                NavigationDestination(icon: Text("Goal"), label: ""),
+                NavigationDestination(icon: Text("Workout"), label: ""),
+              ],
+              selectedIndex: _selectedWorkoutOrGoal,
+              onDestinationSelected: _pressedWorkoutOrGoal,
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),

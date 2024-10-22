1efba8dd399b48285af5fa15fa276092188a8fcc
palm0478 <palm0478@d.umn.edu>
2024-10-22 14:57:58 -0500

    Rebase from Main

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'GetFit',
       theme: ThemeData(
-
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: ''),
+
     );
   }
 }
@@ -43,12 +58,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _selectedPage = 0;
-  int _selectedWorkoutOrGoal = 0;
+  int _selectedWoroutOrGoal = 0;
   String _username = 'USER NAME';
 
   void _incrementCounter() {
     setState(() {
-
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -69,15 +88,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _pressedWorkoutOrGoal(int index) {
     setState(() {
-      _selectedWorkoutOrGoal = index;
+      _selectedWoroutOrGoal = index;
     });
   }
   
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         leading: TextButton(
             onPressed: _pressedAccountButton,
@@ -85,38 +115,18 @@ class _MyHomePageState extends State<MyHomePage> {
             isSemanticButton: true,),
       ),
       body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-
             NavigationBar(
               destinations: [
                 NavigationDestination(icon: Text("Goal"), label: ""),
-                NavigationDestination(icon: Text("Workout"), label: ""),
+                NavigationDestination(icon: Text("Workouts"), label: ""),
               ],
-              backgroundColor: Colors.white,
-              selectedIndex: _selectedWorkoutOrGoal,
+              selectedIndex: _selectedWoroutOrGoal,
               onDestinationSelected: _pressedWorkoutOrGoal,
-            ),
-
-            // switch between Goal page and Workout Page
-            _selectedWorkoutOrGoal == 0
-                  // Goal Page
-                ? Column(
-                    children: <Widget> [
-                      Text("Goal Page", style: TextStyle(fontSize: 24)),
-                      Text("Running Page", style: TextStyle(fontSize: 20)),
-                      Text("Miles Ran: ", style: TextStyle(fontSize: 18)),
-                      Text("Lifting Page", style: TextStyle(fontSize: 20)),
-                      Text("Max lift on Bench: ", style: TextStyle(fontSize: 18))
-                      ]
-                    )
-                  // Workout Page
-                : Column(
-                children: <Widget> [
-                  Text("Workout Page", style: TextStyle(fontSize: 24)),
-                  Text("Workout Page", style: TextStyle(fontSize: 24))
-                ]
             )
           ],
         ),

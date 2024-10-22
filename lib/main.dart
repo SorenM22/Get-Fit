e4df21f59244e0b82faab824881f33d2390c0f72
palm0478 <palm0478@d.umn.edu>
2024-10-22 14:57:58 -0500

    created Goal vs Workouts Widget

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: ''),
+
     );
   }
 }
@@ -57,6 +58,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _selectedPage = 0;
+  int _selectedWoroutOrGoal = 0;
   String _username = 'USER NAME';
 
   void _incrementCounter() {
@@ -82,6 +84,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _pressedAddButton() {
     throw UnimplementedError("Unimplemented _pressedAccountButton");
+  }
+
+  void _pressedWorkoutOrGoal(int index) {
+    setState(() {
+      _selectedWoroutOrGoal = index;
+    });
   }
   
   @override
@@ -110,28 +118,16 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            NavigationBar(
+              destinations: [
+                NavigationDestination(icon: Text("Goal"), label: ""),
+                NavigationDestination(icon: Text("Workouts"), label: ""),
+              ],
+              selectedIndex: _selectedWoroutOrGoal,
+              onDestinationSelected: _pressedWorkoutOrGoal,
+            )
           ],
         ),
       ),

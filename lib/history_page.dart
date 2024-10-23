fdc129cb1dd859b010edcb5a3fc0fb44d7dcf73c
Lucas Moen <moen0348@d.umn.edu>
2024-10-23 15:21:52 -0500

    created history page

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatelessWidget {
-  const HistoryPage({Key? key, required this.title}) : super(key: key);
+  const HistoryPage({super.key, required this.title});
   final String title;
-  
+
+  static const List<String> items = ['0', '1', '2', '3', '4'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {},
-          child: const Text('Go Back'),
-        ),
-      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: ListItem(name: items[index]),
+          );
+        },
+      )
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  const ListItem({super.key, required this.name});
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: Text(name)),
+        const Expanded(child: Text('This is another text input'))
+      ]
     );
   }
 }

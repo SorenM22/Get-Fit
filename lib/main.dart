caf6e47addd0961da9db67d2a6b9ed2c77cb3d28
palm0478 <palm0478@d.umn.edu>
2024-10-27 14:46:05 -0500

    Rebase Main to Parker Testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ctrl_alt_defeat/goal_workout_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -35,21 +36,9 @@ class MyHomePage extends StatefulWidget {
 
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   int _selectedPage = 0;
   int _selectedWorkoutOrGoal = 0;
   String _username = 'USER NAME';
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   void _tappedPageSelect(int index) {
     setState(() {
@@ -73,49 +62,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         leading: TextButton(
             onPressed: _pressedAccountButton,
             child: Text(_username),
             isSemanticButton: true,),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            NavigationBar(
-              destinations: [
-                NavigationDestination(icon: Text("Goal"), label: ""),
-                NavigationDestination(icon: Text("Workouts"), label: ""),
-              ],
-              selectedIndex: _selectedWorkoutOrGoal,
-              onDestinationSelected: _pressedWorkoutOrGoal,
-            )
-          ],
-        ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Add Stuff',
-        child: const Icon(Icons.add),
-      ),
+      body: GoalWorkoutPage(title: "Goal/Workout"),
 
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[

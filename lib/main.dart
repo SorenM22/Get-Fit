d080346ad0b41b7215fdcf54d5f1bea4b06c183c
palm0478 <palm0478@d.umn.edu>
2024-10-27 14:56:22 -0500

    moved MyHomePage to it's own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:ctrl_alt_defeat/goal_workout_page.dart';
 import 'package:flutter/material.dart';
+import 'package:ctrl_alt_defeat/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,75 +24,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedPage = 0;
-  int _selectedWorkoutOrGoal = 0;
-  String _username = 'USER NAME';
-
-  void _tappedPageSelect(int index) {
-    setState(() {
-      _selectedPage = index;
-    });
-  }
-
-  void _pressedAccountButton() {
-    throw UnimplementedError("Unimplemented _pressedAccountButton");
-  }
-
-  void _pressedAddButton() {
-    throw UnimplementedError("Unimplemented _pressedAccountButton");
-  }
-
-  void _pressedWorkoutOrGoal(int index) {
-    setState(() {
-      _selectedWorkoutOrGoal = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        leading: TextButton(
-            onPressed: _pressedAccountButton,
-            child: Text(_username),
-            isSemanticButton: true,),
-      ),
-      body: GoalWorkoutPage(title: "Goal/Workout"),
-
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.auto_graph_rounded),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'settings',
-          ),
-        ],
-          currentIndex: _selectedPage,
-          selectedItemColor: Theme.of(context).primaryColor,
-          onTap: _tappedPageSelect
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,// This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}

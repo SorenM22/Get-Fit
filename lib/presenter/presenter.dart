72e7b18a64dedd5404ff6c5527c589c560cbc31c
Aidan Carlson <carl6746@umn.edu>
2024-10-28 23:19:04 -0500

    Outline of MVP model

---
+++ lib/presenter/presenter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ctrl_alt_defeat/models/model.dart';
+import 'package:ctrl_alt_defeat/views/view.dart';
+//The inbetween
+class Presenter {
+  Presenter();
+  void buttonPushed() {
+    throw UnimplementedError("Unimplemented _pressedAccountButton");
+  }
+}

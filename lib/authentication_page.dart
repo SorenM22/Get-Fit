67fbea198c3b9bf31838124c49e03d11f84a412c
pfenn021 <pfenn021@d.umn.edu>
2024-10-29 13:01:17 -0500

    user login form prototype has been created.

--- lib/authentication_page.dart
+++ lib/authentication_page.dart
@@ -35,55 +35,57 @@ class AuthenticationPage extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
       var height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Container(
-         padding: const EdgeInsets.all(30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image(image: AssetImage(loginImage), height: height * 0.6),
-             const Column(
-                children: [
-                 Text("Login page in progress", textAlign: TextAlign.center,),
-                 Text("May you have many wonderful workouts in the future", textAlign: TextAlign.center,),
-               ],
-            ),
-            Row(
-              children:[
-                Expanded(child: OutlinedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (context) {
-                          return LoginPage();
-                          },
-                        ),
-                    );
-
-                  },
-                  style: OutlinedButton.styleFrom(
-                    shape: RoundedRectangleBorder(),
-                    foregroundColor: Color(0xFF272727),
-                    side: BorderSide(color: Color(0xFF272727)),
-                    padding: EdgeInsets.symmetric(vertical:30),
-                ), child: const Text("Login")),),
-                const SizedBox(
-                  width: 10.0,
-                ),
-                Expanded(child: ElevatedButton(
-                    onPressed: (){},
-                    style: ElevatedButton.styleFrom(
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+           padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image(image: AssetImage(loginImage), height: height * 0.6),
+               const Column(
+                  children: [
+                   Text("Login page in progress", textAlign: TextAlign.center,),
+                   Text("May you have many wonderful workouts in the future", textAlign: TextAlign.center,),
+                 ],
+              ),
+              Row(
+                children:[
+                  Expanded(child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) {
+                            return LoginPage();
+                            },
+                          ),
+                      );
+      
+                    },
+                    style: OutlinedButton.styleFrom(
                       shape: RoundedRectangleBorder(),
-                      foregroundColor: Color(0xFF434343),
-                      backgroundColor: Color(0XFFb7f2f1),
-                      side: BorderSide(color: Color(0xFF434343)),
+                      foregroundColor: Color(0xFF272727),
+                      side: BorderSide(color: Color(0xFF272727)),
                       padding: EdgeInsets.symmetric(vertical:30),
-                    ),
-                    child: const Text("Sign up")),)
-              ]
-            )
-          ]
+                  ), child: const Text("Login")),),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Expanded(child: ElevatedButton(
+                      onPressed: (){},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(),
+                        foregroundColor: Color(0xFF434343),
+                        backgroundColor: Color(0XFFb7f2f1),
+                        side: BorderSide(color: Color(0xFF434343)),
+                        padding: EdgeInsets.symmetric(vertical:30),
+                      ),
+                      child: const Text("Sign up")),)
+                ]
+              )
+            ]
+          )
         )
-      )
+      ),
     );
   }
   }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,25 +8,68 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("LOGIN"),
-        leading: IconButton(onPressed: (){
-          Navigator.pop(context);
-        }, icon: Icon(Icons.arrow_back)),
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("LOGIN"),
+          leading: IconButton(onPressed: (){
+            Navigator.pop(context);
+          }, icon: Icon(Icons.arrow_back)),
+          ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image(image: AssetImage(loginImage)),
+                const Text("testDescription"),
+                const Text("subTitle"),
+
+                Form(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.person_outline_outlined),
+                              labelText: "E-mail",
+                              hintText: "Enter you e-mail",
+                              border: OutlineInputBorder(),
+                        ),
+                          ),
+                          const SizedBox(height: 10),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.fingerprint),
+                              labelText: "Password",
+                              hintText: "Enter your password",
+                              border: OutlineInputBorder(),
+                              suffixIcon: IconButton(
+                                onPressed: null,
+                                icon: Icon(Icons.remove_red_eye_sharp),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              onPressed: (){}, child: const Text("Forgot Password")),
+                          ),
+                          ElevatedButton(onPressed: (){}, child: Text("Let Me In"))
+                        ],
+                      ),
+                    ),
+                ),
+              ],
+            ),
+          ),
         ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(30),
-          child: Column(
-            children: [
-              Image(image: AssetImage(loginImage)),
-              const Text("testDescription"),
-              const Text("subTitle"),
-            ]
-          )
-        )
-      )
+      ),
     );
   }
 }

0e19b634751c796fd7d963b485dd260c7804cb0a
pfenn021 <pfenn021@d.umn.edu>
2024-10-29 21:45:39 -0500

    Signup name field has been added

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -14,8 +14,57 @@ class SignupPage extends StatelessWidget {
               Navigator.pop(context);
             }, icon: Icon(Icons.arrow_back)),
           ),
+
+          body: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: [
+                  signupForm(),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              label: Text("Name"),
+                              prefixIcon: Icon(
+                                  Icons.person_outline_rounded,
+                                  color: Colors.grey),
+                              labelStyle: TextStyle(color: Colors.grey),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 1.7, color: Colors.grey),
+                              )
+                            ),
+                        )
+                        ],
+                      ),
+                    )
+                  )
+                ]
+              )
+            )
+          )
         ),
     );
   }
+}
 
+
+class signupForm extends StatelessWidget {
+  const signupForm ({super.key});
+
+@override
+Widget build(BuildContext context) {
+
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(image: AssetImage("assets/login_Image/Workout_ClipArt.jpg")),
+        Text("test"),
+      ],
+    );
+}
 }

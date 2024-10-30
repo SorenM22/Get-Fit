9e3ef42157c24117a581f1c6a53c302201aa6a2c
Aidan Carlson <carl6746@umn.edu>
2024-10-29 23:19:21 -0500

    Reverted home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:ctrl_alt_defeat/history_page.dart';
 import 'package:flutter/material.dart';
 import 'package:ctrl_alt_defeat/goal_workout_page.dart';
-import 'profile_page.dart';
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -18,6 +16,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedPage = 0;
   int _selectedWorkoutOrGoal = 0;
+  String _username = 'USER NAME';
 
   Widget homeContentWindow = GoalWorkoutPage(title: "Goal/Workout Page");
 
@@ -50,16 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
       _selectedWorkoutOrGoal = index;
     });
   }
-  void _onProfileSelected() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const ProfilePage()),
-    );
-  }
-
-  void _onLogoutSelected() {
-    throw('Logout');
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,61 +56,33 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-        leading: PopupMenuButton<String>(
-          onSelected: (value) {
-            if (value == 'profile') {
-              _onProfileSelected();
-            } else if (value == 'logout') {
-              _onLogoutSelected();
-            }
-          },
-          itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-            const PopupMenuItem<String>(
-              value: 'profile',
-              child: Text('Your Profile'),
-            ),
-            const PopupMenuItem<String>(
-              value: 'logout',
-              child: Text('Log Out'),
-            ),
-          ],
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:  CircleAvatar(
-              radius: 25,
-              backgroundColor: Colors.blue, // Circle color
-              child: const Text('P',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ),
-        ),
+        leading: TextButton(
+          onPressed: _pressedAccountButton,
+          child: Text(_username),
+          isSemanticButton: true,),
       ),
       body: homeContentWindow,
+
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.auto_graph_rounded),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: _selectedPage,
-        selectedItemColor: Theme.of(context).primaryColor,
-        onTap: _tappedPageSelect,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.auto_graph_rounded),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'settings',
+            ),
+          ],
+          currentIndex: _selectedPage,
+          selectedItemColor: Theme.of(context).primaryColor,
+          onTap: _tappedPageSelect
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-

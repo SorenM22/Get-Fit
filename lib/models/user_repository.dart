44fb2c7bc47c81e1a73013669107978aad8d2848
pfenn021 <pfenn021@d.umn.edu>
2024-11-02 17:42:42 -0500

    user signup model created, functionality to add signup info to database

---
+++ lib/models/user_repository.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ctrl_alt_defeat/models/user_model.dart';
+import 'package:get/get.dart';
+
+class UserRepository extends GetxController{
+  static UserRepository get instance => Get.find();
+
+  final _db = FirebaseFirestore.instance;
+  
+  createUser(UserModel user) async {
+   await _db.collection("User_Data").add(user.userDataToJson()).catchError((error) {
+      print(error.toString());
+    });
+
+
+  }
+
+}

139278eb37a324806e1f12255b6506b0bec5ce25
pfenn021 <pfenn021@d.umn.edu>
2024-11-04 12:43:09 -0600

    altered previous login function for uniformity with new google log in function.

--- lib/presenter/login_presenter.dart
+++ lib/presenter/login_presenter.dart
@@ -4,22 +4,34 @@ import 'package:flutter/material.dart';
 
 class loginPresenter extends GetxController {
   static loginPresenter get instance => Get.find();
-  final testingAuth = Get.put(AuthenticationRepository());
+  final authRepo = Get.put(AuthenticationRepository());
 
   final email = TextEditingController();
   final password = TextEditingController();
 
   final isGoogleLoading = false.obs;
+  final isLoading = false.obs;
 
 
   void logUserIn(String email, String password){
-    AuthenticationRepository.instance.loginWithEmailandPass(email, password);
+
+    try {
+      isLoading.value = true;
+      authRepo.loginWithEmailandPass(email, password);
+      isLoading.value = false;
+    } catch (e) {
+      print ("Error logging in: $e");
+    }
+
+    isLoading.value = true;
+    authRepo.loginWithEmailandPass(email, password);
+    isLoading.value = false;
   }
 
   Future<void> googleLogin() async {
     try{
       isGoogleLoading.value = true;
-      await testingAuth.signInWithGoogle();
+      await authRepo.signInWithGoogle();
       isGoogleLoading.value = false;
     } catch (e) {
       isGoogleLoading.value = false;

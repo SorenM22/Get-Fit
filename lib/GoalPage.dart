7d3303528a99785d98a0a6c7b0bffcfa62af7204
Parker Vetter <vette107@d.umn.edu>
2024-11-06 15:49:19 -0600

    Adding Goal Text and add button for page

--- lib/GoalPage.dart
+++ lib/GoalPage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:ctrl_alt_defeat/models/user_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class GoalPage extends StatefulWidget{
   @override
@@ -8,16 +13,30 @@ class GoalPage extends StatefulWidget{
 }
 
 class GoalState extends State<GoalPage>{
+  final userRepo = Get.put(UserRepository());
   @override
   Widget build(BuildContext context) {
     return Column(
         children: <Widget> [
+
           Text("Goal Page", style: TextStyle(fontSize: 24)),
           Text("Running Page", style: TextStyle(fontSize: 20)),
           Text("Miles Ran: ", style: TextStyle(fontSize: 18)),
           Text("Lifting Page", style: TextStyle(fontSize: 20)),
-          Text("Max lift on Bench: ", style: TextStyle(fontSize: 18))
+          Text("Max lift on Bench: ", style: TextStyle(fontSize: 18)),
+          ElevatedButton(
+              onPressed: () {
+                goalSubmit();
+          }, 
+          child: Text("Add Goal")
+          ),
         ]
     );
+  }
+
+  void goalSubmit() async {
+    String? userId = userRepo.getCurrentUserUID();
+    final databaseReference = FirebaseFirestore.instance.collection("User_Data");
+    databaseReference.doc(userId).collection("Goal_Data").doc("Test").set({"Test": 1});
   }
 }

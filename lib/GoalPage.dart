86deec937c8eee024ac1468ba8bf18e8d99b6bbd
Parker Vetter <vette107@d.umn.edu>
2024-11-11 11:47:17 -0600

    GoalPage now pulls from the database and will auto update when you add a new goal

--- lib/GoalPage.dart
+++ lib/GoalPage.dart
@@ -5,6 +5,7 @@ import 'package:ctrl_alt_defeat/models/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+final TextEditingController goalController = TextEditingController();
 
 class GoalPage extends StatefulWidget{
   @override
@@ -31,7 +32,7 @@ class GoalState extends State<GoalPage>{
                 child: TextButton(
                   onPressed: () {
                     Navigator.of(context).pop();
-                    _pressedAddButton('Weight');
+                    _showGoalDialog('Weight');
                   },
                   child: Text('Weight'),
                 ),
@@ -40,7 +41,7 @@ class GoalState extends State<GoalPage>{
                 child: TextButton(
                   onPressed: () {
                     Navigator.of(context).pop();
-                    _pressedAddButton('Cardio');
+                    _showGoalDialog('Cardio');
                   },
                   child: Text('Cardio'),
                 ),
@@ -52,9 +53,48 @@ class GoalState extends State<GoalPage>{
     );
   }
 
+  void _showGoalDialog(String type) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Enter your goal"),
+          content: TextField(
+            controller: goalController,
+            decoration: InputDecoration(hintText: "Enter goal for $type"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Submit"),
+              onPressed: () {
+                goalSubmit(type, goalController.text);
+                goalController.clear();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-  void _pressedAddButton(String type) {
-    setState(() {
+  void goalSubmit(String type, String goal) async {
+    String? userId = userRepo.getCurrentUserUID();
+    final databaseReference = FirebaseFirestore.instance.collection("User_Data");
+    databaseReference
+        .doc(userId)
+        .collection("Goal_Data")
+        .doc(type)
+        .collection(type)
+        .add({"Goal": goal});
+
+    /*setState(() {
       final Map<String, List<Widget>> itemMap = {
         'Cardio': cardioGoals,
         'Weight': weightGoals,
@@ -62,31 +102,53 @@ class GoalState extends State<GoalPage>{
 
       final list = itemMap[type];
       if (list != null) {
-        list.add(GoalWidget(name: '$type ${list.length + 1}'));
+        list.add(GoalWidget(name: '$type $goal'));
       }
-    });
+    });*/
   }
+
   @override
   Widget build(BuildContext context) {
-    return
-      Column(
-          children: <Widget> [
-            Center(child: Text("Cardio Goals", style: TextStyle(fontSize: 24))),
-            Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: cardioGoals.length,
-                  itemBuilder: (context, index) => cardioGoals[index],
-                )
+    return Column(
+        children: <Widget>[
+          Center(child: Text("Cardio Goals", style: TextStyle(fontSize: 24))),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("User_Data")
+                  .doc(userRepo.getCurrentUserUID())
+                  .collection("Goal_Data")
+                  .doc("Cardio")
+                  .collection("Cardio")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return CircularProgressIndicator();
+                final goals = snapshot.data!.docs.map((doc) {
+                  return GoalWidget(name: doc["Goal"]);
+                }).toList();
+                return ListView(children: goals);
+              },
             ),
-            Center(child: Text("Weight Goals", style: TextStyle(fontSize: 24))),
-            Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: weightGoals.length,
-                  itemBuilder: (context, index) => weightGoals[index],
-                )
+          ),
+          Center(child: Text("Weight Goals", style: TextStyle(fontSize: 24))),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("User_Data")
+                  .doc(userRepo.getCurrentUserUID())
+                  .collection("Goal_Data")
+                  .doc("Weight")
+                  .collection("Weight")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return CircularProgressIndicator();
+                final goals = snapshot.data!.docs.map((doc) {
+                  return GoalWidget(name: doc["Goal"]);
+                }).toList();
+                return ListView(children: goals);
+              },
             ),
+          ),
             ElevatedButton(
                 onPressed: _showOptionsForAdd,
                 child: Text("Add Goal")
@@ -95,14 +157,7 @@ class GoalState extends State<GoalPage>{
       );
   }
 
-
-  void goalSubmit() async {
-    String? userId = userRepo.getCurrentUserUID();
-    final databaseReference = FirebaseFirestore.instance.collection("User_Data");
-    databaseReference.doc(userId).collection("Goal_Data").doc("Test").set({"Test": 1});
-  }
 }
-
 
 class GoalWidget extends StatelessWidget {
   const GoalWidget({super.key,required this.name});
